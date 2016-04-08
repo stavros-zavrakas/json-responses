c6bdba4c74bcce4ad9350be97d99ee403dd6981a
Stavros Zavrakas <szavrakas@cambridge.org>
2016-04-08 19:17:48 +0100

    minor fixes on var presence

--- test/aop-ajax-responses.spec.js
+++ test/aop-ajax-responses.spec.js
@@ -3,46 +3,50 @@ var proxyquire = require('proxyquire');
 var sinon = require('sinon');
 var _ = require('lodash');
 
-var constants = require('../constants');
+// var constants = require('../constants');
 
-var typesToCheck = [
-  'string',
-  null,
-  undefined,
-  NaN,
-  Infinity, {},
-  [],
-  /.+/,
-  1,
-  new Date()
-];
+var noop = function () {};
+
+var typesToCheck = {
+  STRING: 'string',
+  NUMBER: 0,
+  NULL: null,
+  UNDEFINED: undefined,
+  NOT_A_NUMBER: NaN,
+  INFINITY: Infinity,
+  OBJECT: {},
+  ARRAY: [],
+  REGEXP: /.+/,
+  DATE: new Date(),
+  BOOLEAN_TRUE: true,
+  BOOLEAN_FALSE: false,
+  FUNCTION: noop
+};
 
 var lib;
 var data;
 var error;
 var getResObj;
-var sendSuccessResponse;
-var sendErrorResponse;
 
-var req = {
-  params: {}
-};
+// var req = {
+//   params: {}
+// };
 
-var res = {};
+// var res = {};
 
-_.extend(res, {
-  status: sinon.stub().returns(res),
-  setHeader: sinon.spy(),
-  json: sinon.stub().returns(res)
-});
+// _.extend(res, {
+//   status: sinon.stub().returns(res),
+//   setHeader: sinon.spy(),
+//   json: sinon.stub().returns(res)
+// });
 
-function getLastReturnResult(spy) {
-  return spy.returnValues.slice(-1)[0];
-}
+// function getLastReturnResult(spy) {
+//   return spy.returnValues.slice(-1)[0];
+// }
 
-function getLastCallArgs(spy) {
-  return spy.args.slice(-1)[0];
-}
+// function getLastCallArgs(spy) {
+//   return spy.args.slice(-1)[0];
+// }
 
 describe('The aop-ajax-responses module', function () {
 
@@ -54,6 +58,10 @@ describe('The aop-ajax-responses module', function () {
     // @todo: stub i18n-future
     lib = proxyquire('../lib/index', {
 
+    });
+
+    lib = lib({
+      baseDir: 'baseDir'
     });
   });
 
@@ -67,83 +75,57 @@ describe('The aop-ajax-responses module', function () {
       getResObj.restore();
     });
 
-    it('should only allow a boolean isSuccess parameter', function () {
-      typesToCheck.forEach(function (type) {
-        try {
-          getResObj(type);
-        } catch (e) {
+    it('data or error must be present', function () {
+      var typesExceptEmpty = _(typesToCheck).pick(['NULL', 'UNDEFINED']).values().value();
 
-        }
+      typesExceptEmpty.forEach(function (type) {
+        try {
+          getResObj(type, type);
+        } catch (e) {}
       });
 
-      assert.strictEqual(getResObj.callCount, typesToCheck.length);
+      assert.strictEqual(getResObj.callCount, _.size(typesExceptEmpty));
+      assert.strictEqual(getResObj.alwaysThrew('TypeError'), true);
+    });
+
+    it('data and error can not be present together', function () {
+      var typesExceptEmpty = _(typesToCheck).omit(['NULL', 'UNDEFINED']).values().value();
+
+      typesExceptEmpty.forEach(function (type) {
+        try {
+          getResObj(type, type);
+        } catch (e) {}
+      });
+
+      assert.strictEqual(getResObj.callCount, _.size(typesExceptEmpty));
       assert.strictEqual(getResObj.alwaysThrew('TypeError'), true);
     });
 
     it('should only allow a string errCode parameter', function () {
-      _.without(typesToCheck, 'string').forEach(function (type) {
+      var someTypes = _(typesToCheck).omit(['NULL', 'UNDEFINED', 'NUMBER', 'NOT_A_NUMBER', 'BOOLEAN_FALSE']).values().value();
+
+      someTypes.forEach(function (type) {
         try {
-          getResObj(false, null, type, 'whatever');
+          getResObj(type);
         } catch (e) {}
       });
 
-      assert.strictEqual(getResObj.callCount, typesToCheck.length - 1);
+      assert.strictEqual(getResObj.callCount, _.size(someTypes));
       assert.strictEqual(getResObj.alwaysThrew('TypeError'), true);
     });
 
-    it('should only allow a string errMsg parameter', function () {
-      _.without(typesToCheck, 'string').forEach(function (type) {
+    it('should only allow an object as error parameter', function () {
+      var allTypes = _(typesToCheck).values().value();
+
+      allTypes.forEach(function (type) {
         try {
-          getResObj(false, null, error.code, type);
+          getResObj(type);
         } catch (e) {}
       });
 
-      assert.strictEqual(getResObj.callCount, typesToCheck.length - 1);
+      assert.strictEqual(getResObj.callCount, _.size(allTypes));
       assert.strictEqual(getResObj.alwaysThrew('TypeError'), true);
     });
 
-    it('should provide a frozen object', function () {
-      try {
-        var resObj = getResObj(true, data);
-        resObj.success = false;
-      } catch (e) {
-        assert.instanceOf(e, Error);
-      }
-    });
-
   });
-
-  describe('when calling sendSuccessResponse', function () {
-    beforeEach(function () {
-      sendSuccessResponse = sinon.spy(lib, 'sendSuccessResponse');
-      res.json.reset();
-    });
-
-    after(function () {
-      sendSuccessResponse.restore();
-    });
-
-    it('sets the correct status code on the request', function () {
-      sendSuccessResponse(req, res, data);
-      assert.strictEqual(getLastCallArgs(res.status)[0], constants.HTTP_CODES.OK);
-    });
-
-  });
-
-  describe('when calling sendErrorResponse', function () {
-    beforeEach(function () {
-      sendErrorResponse = sinon.spy(lib, 'sendErrorResponse');
-      res.json.reset();
-    });
-
-    after(function () {
-      sendErrorResponse.restore();
-    });
-
-    it('sets the correct status code on the request', function () {
-      sendErrorResponse(req, res, data);
-      assert.strictEqual(getLastCallArgs(res.status)[0], constants.HTTP_CODES.OK);
-    });
-  });
-
 });
